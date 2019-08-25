9951c931eb01d3db34ed5a0bd71aaa3c3071c199
etrapos <etrapos@yahoo.co.jp>
2019-08-25 18:24:31 +0900

    change path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v /Users/tanaka-yu/work/study/ar.js/html:/usr/share/nginx/html:ro -p 8080:80 nginx
+docker run --rm -v /home/ec2-user/environment/arjs/html:/usr/share/nginx/html:ro -p 80:80 nginx
